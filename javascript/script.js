941342d58b242219061d85e8418385cf586b844c
Rifqi Favian <piyanh90@gmail.com>
2023-03-14 08:48:36 +0700

    maintain code readable

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  popularComicRender();
+  popularComic();
 });
 
 // Slider
@@ -30,122 +30,151 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// PopularComicRender
-let popularComicRender = () => {
-  let popularComic = fetch("https://komiku-api.fly.dev/api/comic/popular/page/1");
-  popularComic
+// show popular comic
+const popularComic = async () => {
+  try {
+    const dataPopularComic = await getPopularComic();
+    renderPopularComic(dataPopularComic);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getPopularComic = () => {
+  return fetch("https://komiku-api.fly.dev/api/comic/popular/page/1")
     .then((response) => response.json())
-    .then((response) => {
-      const comics = response.data;
-      let result = "";
-      comics.map((comic) => {
-        let comics = `<div class="swiper-slide">
-                        <div class="card" style="width: 18rem">
-                          <img src="${comic.image}" class="card-img-top" alt="..." />
-                          <div class="card-body">
-                            <h5 class="card-title">Title : ${comic.title}</h5>
-                            <h6>Type : ${comic.type}</h6>
-                            <p class="card-text">Deskripsi : ${comic.desc}</p>
-                            <a href="#" class="btn btn-primary button-info" data-endpoint="${comic.endpoint}">
-                              Baca ngabs
-                            </a>
+    .then((response) => response.data);
+};
+const renderPopularComic = (datas) => {
+  let result = "";
+  datas.map((data) => {
+    result += `<div class="swiper-slide">
+                          <div class="card" style="width: 18rem">
+                            <img src="${data.image}" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                              <h5 class="card-title">Title : ${data.title}</h5>
+                              <h6>Type : ${data.type}</h6>
+                              <p class="card-text">Deskripsi : ${data.desc}</p>
+                              <a href="#" class="btn btn-primary button-info" data-endpoint="${data.endpoint}">
+                                Baca ngabs
+                              </a>
+                            </div>
                           </div>
-                        </div>
-                      </div>`;
-        result += comics;
-        document.querySelector(".swiper-wrapper").innerHTML = result;
-      });
-    });
+                        </div>`;
+    document.querySelector(".swiper-wrapper").innerHTML = result;
+  });
 };
 
-// Get Query Render
+// show manga from query
 const searchButton = document.querySelector(".submit-query");
-const mangaContainer = document.querySelector(".manga");
 searchButton.addEventListener("click", () => {
   const titleQuery = document.querySelector(".title-query").value;
   if (!titleQuery) {
     alert("Kata kunci belum dimasukkan");
     window.location.reload();
   }
-  renderComicQuery(titleQuery);
+  renderKeywords(titleQuery);
 });
-let renderComicQuery = (titleQuery) => {
-  return fetch(`https://komiku-api.fly.dev/api/comic/search/${titleQuery}`)
-    .then((e) => e.json())
-    .then((e) => {
-      if (!e.success) {
-        return alert("masukin kata kunci yang bener blok!");
+
+const renderKeywords = async (keywords) => {
+  try {
+    const dataComicQuery = await getComicQuery(keywords);
+    renderComicQuery(dataComicQuery);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getComicQuery = (keywords) => {
+  return fetch(`https://komiku-api.fly.dev/api/comic/search/${keywords}`)
+    .then((response) => response.json())
+    .then((response) => {
+      if (!response.success) {
+        alert("masukin kata kunci yang bener blok!");
       }
-      const queryComics = e.data;
-      mangaContainer.innerHTML = `<h3>Hasil Pencarian :</h3>
-                                  <div class='row'>
-                                    <div class='col-md container-query'></div>
-                                  </div>`;
-      let queryResult = "";
-      queryComics.map((queryC) => {
-        const queryComs = `<div class="card" style="width: 18rem">
-                              <img src="${queryC.image}" class="card-img-top" alt="..." />
-                              <div class="card-body">
-                                <h5 class="card-title">Title : ${queryC.title}</h5>
-                                <h6>Type : ${queryC.type}</h6>
-                                <p class="card-text">Deskripsi : ${queryC.desc}</p>
-                                <a href="#" class="btn btn-primary button-info" data-endpoint="${queryC.endpoint}">
-                                  Baca ngabs
-                                </a>
-                              </div>
-                            </div>`;
-        queryResult += queryComs;
-        document.querySelector(".container-query").innerHTML = queryResult;
-      });
+      return response.data;
     });
 };
 
-// get Info Manga (Event Binding)
-document.body.addEventListener("click", function (e) {
+const renderComicQuery = (datas) => {
+  const mangaContainer = document.querySelector(".manga");
+  mangaContainer.innerHTML = `<h3>Hasil Pencarian :</h3>
+                              <div class='row'>
+                                <div class='col-md container-query'></div>
+                              </div>`;
+  let queryResult = "";
+  datas.map((data) => {
+    queryResult += `<div class="card" style="width: 18rem">
+                      <img src="${data.image}" class="card-img-top" alt="..." />
+                      <div class="card-body">
+                        <h5 class="card-title">Title : ${data.title}</h5>
+                        <h6>Type : ${data.type}</h6>
+                        <p class="card-text">Deskripsi : ${data.desc}</p>
+                        <a href="#" class="btn btn-primary button-info" data-endpoint="${data.endpoint}">
+                        Baca ngabs
+                        </a>
+                      </div>
+                    </div>`;
+    document.querySelector(".container-query").innerHTML = queryResult;
+  });
+};
+
+// show info manga (Event Binding)
+document.body.addEventListener("click", async function (e) {
   if (e.target && e.target.classList.contains("button-info")) {
-    fetch(`https://komiku-api.fly.dev/api/comic/info${e.target.dataset.endpoint}`)
-      .then((info) => info.json())
-      .then((info) => {
-        let result = "";
-        mangaContainer.innerHTML = `<div class="manga-info text-center">
-                                    </div>`;
-        const mangaInfo = info.data;
-        console.log(mangaInfo);
-        let chapterList = mangaInfo.chapter_list.map((e) => {
-          return `<li class="manga-chapter" data-chapter="${e.endpoint}">${e.name}</li>`;
-        });
-        result = `<img src="${mangaInfo.thumbnail}" class="img-fluid mthumbnail" alt="manga-thumbnails">
-                  <h4>Title : ${mangaInfo.title}</h4>
-                  <h5>Author : ${mangaInfo.author}</h5>
-                  <h5>Genre : ${mangaInfo.genre.join(", ")}</h5>
-                  <h5>Rating : ${mangaInfo.rating}</h5>
-                  <h5>Status : ${mangaInfo.status}</h5>
-                  <div class="chapter-list">
-                    <ul>
-                      ${chapterList.join("")}
-                    </ul>
-                  </div>
-                  <div class="image-of-manga"></div>`;
-
-        document.querySelector(".manga-info").innerHTML = result;
-      });
-  }
-  if (e.target && e.target.classList.contains("manga-chapter")) {
-    console.log(e.target.dataset.chapter);
-    fetch(`https://komiku-api.fly.dev/api/comic/chapter${e.target.dataset.chapter}`)
-      .then((chapters) => chapters.json())
-      .then((chapters) => {
-        console.log(chapters);
-        const mangaChapters = document.querySelector(".image-of-manga");
-        mangaChapters.innerHTML = "";
-        let chapterData = chapters.data;
-        let chapterImg = chapterData.image.map((img) => {
-          return `<img class="img-fluid" src="${img}" alt="image chapters">`;
-        });
-        let manga = `<h5>${chapterData.title}</h5>
-                      ${chapterImg.join("")}`;
-
-        mangaChapters.innerHTML = manga;
-      });
+    const detailComic = await getDetailComic(e.target.dataset.endpoint);
+    renderDetailComic(detailComic);
+  } else if (e.target && e.target.classList.contains("manga-chapter")) {
+    const dataChapter = await getComicPanel(e.target.dataset.chapter);
+    renderComicPanel(dataChapter);
   }
 });
+
+// show comic panel
+const getComicPanel = (chapterEndpoint) => {
+  return fetch(`https://komiku-api.fly.dev/api/comic/chapter${chapterEndpoint}`)
+    .then((panels) => panels.json())
+    .then((panels) => panels.data);
+};
+
+const renderComicPanel = (panels) => {
+  const mangaChapters = document.querySelector(".image-of-manga");
+  mangaChapters.innerHTML = "";
+  const chapterImg = panels.image.map((img) => {
+    return `<img class="img-fluid" src="${img}" alt="image chapters">`;
+  });
+  const manga = `<h5>${panels.title}</h5>
+                ${chapterImg.join("")}`;
+
+  mangaChapters.innerHTML = manga;
+};
+
+// show detail comic
+const getDetailComic = (detailEndpoint) => {
+  return fetch(`https://komiku-api.fly.dev/api/comic/info${detailEndpoint}`)
+    .then((response) => response.json())
+    .then((response) => response.data);
+};
+
+const renderDetailComic = (detail) => {
+  const mangaContainer = document.querySelector(".manga");
+  mangaContainer.innerHTML = `<div class="manga-info text-center">
+                              </div>`;
+  let result = "";
+  const chapterList = detail.chapter_list.map((e) => {
+    return `<li class="manga-chapter" data-chapter="${e.endpoint}">${e.name}</li>`;
+  });
+  result = `<img src="${detail.thumbnail}" class="img-fluid mthumbnail" alt="manga-thumbnails">
+            <h4>Title : ${detail.title}</h4>
+            <h5>Author : ${detail.author}</h5>
+            <h5>Genre : ${detail.genre.join(", ")}</h5>
+            <h5>Rating : ${detail.rating}</h5>
+            <h5>Status : ${detail.status}</h5>
+            <div class="chapter-list">
+              <ul>
+                ${chapterList.join("")}
+              </ul>
+            </div>
+            <div class="image-of-manga"></div>`;
+
+  document.querySelector(".manga-info").innerHTML = result;
+};
